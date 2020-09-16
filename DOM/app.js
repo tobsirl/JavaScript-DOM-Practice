edff7e70dda1b7e9109ccdc0959489a424bcb43b
Paul Tobin <tobster111@gmail.com>
2020-09-16 20:01:02 +0100

    Using Array.form() to convert a HTML collection

--- DOM/app.js
+++ DOM/app.js
@@ -70,14 +70,30 @@ document.querySelector('li:nth-child(3)').style.color = 'yellow';
 // document.getElementsByClassName
 const items = document.getElementsByClassName('collection-item');
 
-console.log(items);
-console.log(items[0]);
+// console.log(items);
+// console.log(items[0]);
 
-items[0].style.color = 'red';
-items[3].textContent = 'Hello';
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
 
-const listItems = document
-  .querySelector('ul')
-  .getElementsByClassName('collection-item');
+// const listItems = document
+//   .querySelector('ul')
+//   .getElementsByClassName('collection-item');
 
-  console.log(listItems);
+// console.log(listItems);
+
+// document.getElementByTagName
+let lis = document.getElementsByTagName('li');
+
+console.log(lis);
+console.log(lis[0]);
+
+lis[0].style.color = 'red';
+lis[3].textContent = 'Hello';
+
+// Convert HTML Collection into an array using Array.form()
+lis = Array.from(lis);
+
+lis.reverse();
+
+console.log(lis);
