f8f9aa90d1c14b2803c6a617fa32b77ca12c94ff
Paul Tobin <tobster111@gmail.com>
2020-09-19 11:38:26 +0100

    Removing elements using remove() and removeChild()

--- DOM/app.js
+++ DOM/app.js
@@ -232,5 +232,15 @@ const cardAction = document.querySelector('.card-action');
 // Replace
 cardAction.replaceChild(newHeading, oldHeading);
 
-console.log(newHeading);
-console.log(oldHeading);
+// !REMOVE ELEMENT
+const lis = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// Remove list item
+lis[0].remove();
+
+// Remove child element
+list.removeChild(lis[3]);
+
+// !CLASS & ATTR
+
