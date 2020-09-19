25ae68bd8ff2f14aa82c99477d3f0527cd62ab89
Paul Tobin <tobster111@gmail.com>
2020-09-19 11:59:36 +0100

    Setting, removing and replacing classes and attributes

--- DOM/app.js
+++ DOM/app.js
@@ -243,4 +243,25 @@ lis[0].remove();
 list.removeChild(lis[3]);
 
 // !CLASS & ATTR
+const firstLi = document.querySelector('li:first-child');
+const link = firstLi.children[0];
 
+let val;
+
+// Classes
+val = link.className;
+val = link.classList;
+val = link.classList[0];
+link.classList.add('test');
+link.classList.remove('test');
+val = link;
+
+// Attributes
+val = link.getAttribute('href');
+val = link.setAttribute('href', 'http://google.com');
+link.setAttribute('title', 'Google');
+val = link.hasAttribute('href');
+link.removeAttribute('title');
+val = link;
+
+console.log(val);
