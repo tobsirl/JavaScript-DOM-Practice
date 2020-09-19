aeb7c9dbff8599c593d06015829950d18fee919e
Paul Tobin <tobster111@gmail.com>
2020-09-19 10:54:01 +0100

    Constructing an element using createElement, adding className, id and attributes

--- DOM/app.js
+++ DOM/app.js
@@ -124,19 +124,19 @@ document.querySelector('li:nth-child(3)').style.color = 'yellow';
 
 // console.log(items);
 
-let val;
+// let val;
 
-const list = document.querySelector('ul.collection');
-const listItem = document.querySelector('li.collection-item:first-child');
+// const list = document.querySelector('ul.collection');
+// const listItem = document.querySelector('li.collection-item:first-child');
 
-val = listItem;
-val = list;
+// val = listItem;
+// val = list;
 
-// Get child nodes
-val = list.childNodes;
-val = list.childNodes[0];
-val = list.childNodes[0].nodeName;
-val = list.childNodes[1].nodeType;
+// // Get child nodes
+// val = list.childNodes;
+// val = list.childNodes[0];
+// val = list.childNodes[0].nodeName;
+// val = list.childNodes[1].nodeType;
 
 // 1 - Element
 // 2 - Attribute (deprecated)
@@ -146,37 +146,51 @@ val = list.childNodes[1].nodeType;
 // 10 - Doctype
 
 // Get children element nodes
-val = list.children;
-val = list.children[0];
-list.children[1].textContent = 'Hello';
+// val = list.children;
+// val = list.children[0];
+// list.children[1].textContent = 'Hello';
 
 // Children of children
-list.children[3].children[0].id = 'test-link';
-val = list.children[3].children[0];
+// list.children[3].children[0].id = 'test-link';
+// val = list.children[3].children[0];
 
 // first child
-val = list.firstChild;
-val = list.firstElementChild;
+// val = list.firstChild;
+// val = list.firstElementChild;
 
 // last child
-val = list.lastChild;
-val = list.lastElementChild;
+// val = list.lastChild;
+// val = list.lastElementChild;
 
 // Count child elements
-val = list.childElementCount;
+// val = list.childElementCount;
 
 // Get parent node
-val = listItem.parentNode;
-val = listItem.parentElement;
-val = listItem.parentElement.parentElement;
+// val = listItem.parentNode;
+// val = listItem.parentElement;
+// val = listItem.parentElement.parentElement;
 
 // Get next sibling
-val = listItem.nextSibling;
-val = listItem.nextElementSibling;
-val = listItem.nextElementSibling.nextElementSibling;
+// val = listItem.nextSibling;
+// val = listItem.nextElementSibling;
+// val = listItem.nextElementSibling.nextElementSibling;
 
 // Get prev sibling
-val = listItem.previousSibling;
-val = listItem.previousElementSibling;
+// val = listItem.previousSibling;
+// val = listItem.previousElementSibling;
 
-console.log(val);
+// console.log(val);
+
+// Create element
+const li = document.createElement('li');
+
+// Add class
+li.className = 'collection-item';
+
+// Add id
+li.id = 'new-item';
+
+// Add attribute
+li.setAttribute('title', 'New Item');
+
+console.log(li);
