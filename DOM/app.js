a0738f664653028e8a7e065f79f3928aa15bfb80
Paul Tobin <tobster111@gmail.com>
2020-09-20 12:37:25 +0100

    Mouse Events

--- DOM/app.js
+++ DOM/app.js
@@ -277,35 +277,48 @@
 // console.log(btn);
 
 // Named function
-document.querySelector('.clear-tasks').addEventListener('click', onClick);
+// document.querySelector('.clear-tasks').addEventListener('click', onClick);
 
-function onClick(e) {
-  // console.log(`Clicked`);
-  let val;
+// function onClick(e) {
+// console.log(`Clicked`);
+// let val;
 
-  val = e;
+// val = e;
 
-  // Event target element
-  val = e.target;
-  val = e.target.id;
-  val = e.target.className;
-  val = e.target.classList;
+// Event target element
+// val = e.target;
+// val = e.target.id;
+// val = e.target.className;
+// val = e.target.classList;
 
-  // e.target.innerText = "Hello"
+// e.target.innerText = "Hello"
 
-  // Event type
-  val = e.type;
+// Event type
+// val = e.type;
 
-  // Timestamp
-  val = e.timeStamp;
+// Timestamp
+// val = e.timeStamp;
 
-  // Coords event relative to the window
-  val = e.clientY;
-  val = e.clientX;
+// Coords event relative to the window
+// val = e.clientY;
+// val = e.clientX;
 
-  // Coords event relative to the element
-  val = e.offsetY;
-  val = e.offsetX;
+// Coords event relative to the element
+//   val = e.offsetY;
+//   val = e.offsetX;
 
-  console.log(val);
+//   console.log(val);
+// }
+
+// !Mouse events
+const clearBtn = document.querySelector('.clear-tasks');
+const card = document.querySelector('.card');
+const heading = document.querySelector('h5');
+
+// Click
+clearBtn.addEventListener('click', runEvent);
+
+// Event Handler
+function runEvent(e) {
+  console.log(`EVENT TYPE: ${e.type}`);
 }
