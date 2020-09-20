d2b41e47991fad59277a41098e125c096e03702f
Paul Tobin <tobster111@gmail.com>
2020-09-20 12:07:49 +0100

    Example of an click event

--- DOM/app.js
+++ DOM/app.js
@@ -215,53 +215,63 @@
 
 // !REPLACE ELEMENT
 // Create Element
-const newHeading = document.createElement('h2');
+// const newHeading = document.createElement('h2');
 
 // Add ID
-newHeading.id = 'task-title';
+// newHeading.id = 'task-title';
 
 // New text node
-newHeading.appendChild(document.createTextNode('Task List'));
+// newHeading.appendChild(document.createTextNode('Task List'));
 
 // Get the old heading
-const oldHeading = document.getElementById('task-title');
+// const oldHeading = document.getElementById('task-title');
 
 // Parent
-const cardAction = document.querySelector('.card-action');
+// const cardAction = document.querySelector('.card-action');
 
 // Replace
-cardAction.replaceChild(newHeading, oldHeading);
+// cardAction.replaceChild(newHeading, oldHeading);
 
 // !REMOVE ELEMENT
-const lis = document.querySelectorAll('li');
-const list = document.querySelector('ul');
+// const lis = document.querySelectorAll('li');
+// const list = document.querySelector('ul');
 
 // Remove list item
-lis[0].remove();
+// lis[0].remove();
 
 // Remove child element
-list.removeChild(lis[3]);
+// list.removeChild(lis[3]);
 
 // !CLASS & ATTR
-const firstLi = document.querySelector('li:first-child');
-const link = firstLi.children[0];
+// const firstLi = document.querySelector('li:first-child');
+// const link = firstLi.children[0];
 
-let val;
+// let val;
 
 // Classes
-val = link.className;
-val = link.classList;
-val = link.classList[0];
-link.classList.add('test');
-link.classList.remove('test');
-val = link;
+// val = link.className;
+// val = link.classList;
+// val = link.classList[0];
+// link.classList.add('test');
+// link.classList.remove('test');
+// val = link;
 
 // Attributes
-val = link.getAttribute('href');
-val = link.setAttribute('href', 'http://google.com');
-link.setAttribute('title', 'Google');
-val = link.hasAttribute('href');
-link.removeAttribute('title');
-val = link;
+// val = link.getAttribute('href');
+// val = link.setAttribute('href', 'http://google.com');
+// link.setAttribute('title', 'Google');
+// val = link.hasAttribute('href');
+// link.removeAttribute('title');
+// val = link;
 
-console.log(val);
+// console.log(val);
+
+// !Event Listeners and The Event Object
+const btn = document.querySelector('.clear-tasks');
+
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  console.log(`Click`);
+});
+
+console.log(btn);
