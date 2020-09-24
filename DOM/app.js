001592905d3473f7c0d12a747612ffee9ee1ea44
Paul Tobin <tobster111@gmail.com>
2020-09-24 19:29:50 +0100

    Example of event delegation

--- DOM/app.js
+++ DOM/app.js
@@ -396,18 +396,36 @@ taskInput.value = '';
 // e.preventDefault();
 
 // Event Bubbling
-document
-  .querySelector('.card-title')
-  .addEventListener('click', () => console.log('card title'));
+// document
+//   .querySelector('.card-title')
+//   .addEventListener('click', () => console.log('card title'));
 
-document
-  .querySelector('.card-content')
-  .addEventListener('click', () => console.log('card content'));
+// document
+//   .querySelector('.card-content')
+//   .addEventListener('click', () => console.log('card content'));
 
-document
-  .querySelector('.card')
-  .addEventListener('click', () => console.log('card'));
+// document
+//   .querySelector('.card')
+//   .addEventListener('click', () => console.log('card'));
 
-document
-  .querySelector('.col')
-  .addEventListener('click', () => console.log('col'));
+// document
+//   .querySelector('.col')
+//   .addEventListener('click', () => console.log('col'));
+
+// Event Delegation
+// const delItem = document.querySelector('.delete-item');
+
+// delItem.addEventListener('click', deleteItem);
+
+document.body.addEventListener('click', deleteItem);
+
+function deleteItem(e) {
+  // if (e.target.parentElement.className === 'delete-item secondary-content') {
+  //   console.log(`delete item`);
+  // }
+
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    console.log(`delete item`);
+    e.target.parentElement.parentElement.remove();
+  }
+}
